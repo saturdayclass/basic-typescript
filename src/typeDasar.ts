bbd8e5ace11ced9f311e28bc5e4b8924e4c6d455
raihan-muhammad <raihanmuhammad.dev@gmail.com>
2020-10-06 20:33:33 +0700

    add: added example with number and boolean data type

--- src/typeDasar.ts
+++ src/typeDasar.ts
@@ -1,6 +1,16 @@
 // Typescript adalah static typeing
 // Typescript juga sangat strict
 
-// Contoh variable dengan bertipe data string
 // Jika di rubah nilainya selain string maka di akan error
+// Contoh penggunaan Typescript dengan beberapa tipe data yang sering di gunakan
+
+// String
 let nama: string = 'Nama';
+
+// Number
+let age: number;
+age = 17;
+
+// Boolean
+let isMerried: boolean;
+isMerried = false;
