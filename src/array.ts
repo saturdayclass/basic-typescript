14952771355f6b9f0fb6fac08e152fc83456b4a4
raihan-muhammad <raihanmuhammad.dev@gmail.com>
2020-10-06 21:23:37 +0700

    add: added example using array tuples in typescript

--- src/array.ts
+++ src/array.ts
@@ -10,3 +10,14 @@ array2 = ['a', 'b', 'c'];
 // Jika ingin array nya bisa di isi semua type data, kita bisa gunakan any seperti di bawah ini
 let array3: any[];
 array3 = ['1', 2, false, {}, ['aa', 11]];
+
+/* 
+Tuples
+Adalah sebuah array yang tipe ny bisa berbagai macam tipe data tetapi terbatas.
+*/
+
+let biodata: [string, number];
+biodata = ['Raihan', 19];
+
+// Tidak bisa
+// biodata = [19, 'surakarta', false];
