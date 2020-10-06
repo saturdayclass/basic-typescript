384dabfec298843578d566cee03049e9d88e6b53
raihan-muhammad <raihanmuhammad.dev@gmail.com>
2020-10-06 16:08:06 +0700

    add: added folder src and file typeDasar

---
+++ src/typeDasar.ts
@@ -0,0 +1,6 @@
+// Typescript adalah static typeing
+// Typescript juga sangat strict
+
+// Contoh variable dengan bertipe data string
+// Jika di rubah nilainya selain string maka di akan error
+let nama: string = 'Nama';
