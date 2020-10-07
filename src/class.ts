863b6f72aa11afc5a09f0f865af7e0815bcf8bf1
raihan-muhammad <raihanmuhammad.dev@gmail.com>
2020-10-07 22:47:00 +0700

    add: example using super constructor in typescript

--- src/class.ts
+++ src/class.ts
@@ -19,15 +19,25 @@ export class User {
   };
 }
 
+// Public = bisa di akses di semua class atau dari luar class
+// protected = hanya bisa di akses oleh class tsb dan class turunanya
+// private = hanya bisa di akses oleh class itu sendiri
+
 class Admin extends User {
   read: boolean = true;
   write: boolean = true;
+  phone: string;
+
+  constructor(name: string, age: number, phone: string) {
+    super(name, age);
+    this.phone = phone;
+  }
 
   getRole(): { read: boolean; write: boolean } {
     return { read: this.read, write: this.write };
   }
 }
 
-let admin = new Admin('Raihan', 19);
+let admin = new Admin('Raihan', 19, '628737367676');
 console.log(admin.getName());
 console.log(admin.getRole());
